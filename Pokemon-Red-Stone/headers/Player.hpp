cf6645dfec067f279a5390b3c6e58135e46f69b6
Arthur <45017124+Falaron@users.noreply.github.com>
2022-11-24 12:52:39 +0100

    ready to setup player movement

--- Pokemon-Red-Stone/headers/Player.hpp
+++ Pokemon-Red-Stone/headers/Player.hpp
@@ -1,16 +1,15 @@
 //#include <SFML/Graphics.hpp>
-//#include "AnimatedEntity.hpp"
 //
-//class Player : public AnimatedEntity 
+//class Player
 //{
 //public:
 //	Player(const sf::Texture &texture);
-//	//Player();
-//
-//	void PlayerMove(const float& data, const float positionX, const float positionY);
+//	void PlayerMove(const float& dt, const float positionX, const float positionY);
+// 
 //private:
 //	sf::Texture PlayerTexture;
 //	sf::Sprite PlayerSprite;
+// 
 //	int positionX = 0;
 //	int positionY = 0;
 //	float speed = 100;
