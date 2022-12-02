4dc05b5fc8af6391a5562c4f15fe83747bc2d86e
Valentin Faguet <valentin@wallmatt.com>
2022-12-02 01:48:08 +0100

    removing weird files

--- Pokemon-Red-Stone/src/map/houseG.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="houseG" tilewidth="16" tileheight="16" tilecount="98" columns="7">
- <image source="C:/Users/VOGT/Desktop/Jeu Python/Python-Game/interior/basics/stairs.png" trans="003039" width="112" height="224"/>
-</tileset>
